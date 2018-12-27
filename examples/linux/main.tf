f2593a21f3527b3041ce9bfd65e85a55bb9c3f45
arman <arman@keyoumarsi.com>
2018-12-27 14:05:14 +0100

    Added LICENSE and Fixed Example Formating

--- examples/linux/main.tf
+++ examples/linux/main.tf
@@ -1,13 +1,13 @@
 module "example-server-linux" {
-  source    = "Module Source"                 
-  version = "v0.3.0"
-  vmtemp    = "TemplateName"           
-  instances = 1                        
-  vmname    = "example-server-windows" 
-  vmrp     = "esxi/Resources"  
+  source            = "Module Source"                 
+  version           = "v0.3.0"
+  vmtemp            = "TemplateName"           
+  instances         = 1                        
+  vmname            = "example-server-windows" 
+  vmrp              = "esxi/Resources"  
   vlan              = "Name of the VLAN in vSphere"
   data_disk         = "true"
   data_disk_size_gb = 20
   dc                = "Datacenter"
-  ds_cluster = "DEEDC-CS-ECOM-DEV"
+  ds_cluster        = "DEEDC-CS-ECOM-DEV"
 }
--- examples/windows/main.tf
+++ examples/windows/main.tf
@@ -1,14 +1,14 @@
 module "example-server-windows-withdatadisk" {
-  source    = "Module Source"                 
-  version = "v0.3.0"
-  vmtemp    = "TemplateName"           
-  instances = 1                        
-  vmname    = "example-server-windows" 
-  vmrp     = "esxi/Resources"  
+  source            = "Module Source"                 
+  version           = "v0.3.0"
+  vmtemp            = "TemplateName"           
+  instances         = 1                        
+  vmname            = "example-server-windows" 
+  vmrp              = "esxi/Resources"  
   vlan              = "Name of the VLAN in vSphere"
   data_disk         = "true"
   data_disk_size_gb = 20
   is_windows_image  = "true"
   dc                = "Datacenter"
-  ds_cluster = "DEEDC-CS-ECOM-DEV"
+  ds_cluster        = "DEEDC-CS-ECOM-DEV"
 }
