3e4328bb2f0b21bf8bd058cb9e04bb65015c15f8
Arman Keyoumarsi <arman@example.com>
2019-08-08 12:22:15 +0200

    updated output with new vars

--- output.tf
+++ output.tf
@@ -7,6 +7,42 @@ output "ResPool_ID" {
   value       = "${data.vsphere_resource_pool.pool.id}"
 }
 
-output "debug" {
-  value = "test"
+output "Windows-VM" {
+  description = "VM Names"
+  value = ["${vsphere_virtual_machine.Windows.*.name}"]
+}
+
+output "Windows-ip" {
+  description = "default ip address of the deployed VM"
+  value = ["${vsphere_virtual_machine.Windows.*.default_ip_address}"]
+}
+
+output "Windows-guest-ip" {
+  description = "all the registered ip address of the VM"
+  value = ["${vsphere_virtual_machine.Windows.*.guest_ip_addresses}"]
+}
+
+output "Windows-uuid" {
+  description = "UUID of the VM in vSphere"
+  value = ["${vsphere_virtual_machine.Windows.*.uuid}"]
+}
+
+output "Linux-VM" {
+  description = "VM Names"
+  value = ["${vsphere_virtual_machine.Linux.*.name}"]
+}
+
+output "Linux-ip" {
+  description = "default ip address of the deployed VM"
+  value = ["${vsphere_virtual_machine.Linux.*.default_ip_address}"]
+}
+
+output "Linux-guest-ip" {
+  description = "all the registered ip address of the VM"
+  value = ["${vsphere_virtual_machine.Linux.*.guest_ip_addresses}"]
+}
+
+output "Linux-uuid" {
+  description = "UUID of the VM in vSphere"
+  value = ["${vsphere_virtual_machine.Linux.*.uuid}"]
 }
