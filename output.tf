317904cc32dc5c8abc529dff12ea29fe3f781183
Arman Keyoumarsi <arman@keyoumarsi.com>
2020-05-13 10:42:54 +0100

    Revert "* Updated outputs for v12"
    > reverting back to v1.0.2 due to faulty release
    
    This reverts commit ce58b92bdcb06bfa8b7a2940a9d29d82eff3277d.

--- output.tf
+++ output.tf
@@ -1,49 +1,48 @@
 output "DC_ID" {
   description = "id of vSphere Datacenter"
-  value       = data.vsphere_datacenter.dc.id
+  value       = "${data.vsphere_datacenter.dc.id}"
 }
 output "ResPool_ID" {
   description = "Resource Pool id"
-  value       = data.vsphere_resource_pool.pool.id
+  value       = "${data.vsphere_resource_pool.pool.id}"
 }
 
 output "Windows-VM" {
   description = "VM Names"
-  value       = vsphere_virtual_machine.Windows.*.name
+  value       = ["${vsphere_virtual_machine.Windows.*.name}"]
 }
 
 output "Windows-ip" {
   description = "default ip address of the deployed VM"
-  value       = vsphere_virtual_machine.Windows.*.default_ip_address
+  value       = ["${vsphere_virtual_machine.Windows.*.default_ip_address}"]
 }
 
 output "Windows-guest-ip" {
   description = "all the registered ip address of the VM"
-  value       = vsphere_virtual_machine.Windows.*.guest_ip_addresses
+  value       = ["${vsphere_virtual_machine.Windows.*.guest_ip_addresses}"]
 }
 
 output "Windows-uuid" {
   description = "UUID of the VM in vSphere"
-  value       = vsphere_virtual_machine.Windows.*.uuid
+  value       = ["${vsphere_virtual_machine.Windows.*.uuid}"]
 }
 
 output "Linux-VM" {
   description = "VM Names"
-  value       = vsphere_virtual_machine.Linux.*.name
+  value       = ["${vsphere_virtual_machine.Linux.*.name}"]
 }
 
 output "Linux-ip" {
   description = "default ip address of the deployed VM"
-  value       = vsphere_virtual_machine.Linux.*.default_ip_address
+  value       = ["${vsphere_virtual_machine.Linux.*.default_ip_address}"]
 }
 
 output "Linux-guest-ip" {
   description = "all the registered ip address of the VM"
-  value       = vsphere_virtual_machine.Linux.*.guest_ip_addresses
+  value       = ["${vsphere_virtual_machine.Linux.*.guest_ip_addresses}"]
 }
 
 output "Linux-uuid" {
   description = "UUID of the VM in vSphere"
-  value       = vsphere_virtual_machine.Linux.*.uuid
+  value       = ["${vsphere_virtual_machine.Linux.*.uuid}"]
 }
-
