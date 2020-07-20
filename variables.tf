89233b8f3719608fc59ab4c2a52ec6d33d16412f
Samy Coenen <samy.coenen@nubera.be>
2020-07-20 10:39:29 +0200

    Update default to null

--- variables.tf
+++ variables.tf
@@ -197,7 +197,7 @@ variable "data_disk_scsi_controller" {
 variable "scsi_bus_sharing" {
   description = "scsi_bus_sharing mode, acceptable values physicalSharing,virtualSharing,noSharing"
   type        = string
-  default     = "noSharing"
+  default     = null
 }
 
 variable "scsi_type" {
