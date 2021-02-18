5f615bcd5f198913a89a803f7e882ea257ac5ab4
Ryan Bartram <ex.ryan.bartram@elipslife.com>
2021-02-18 23:24:33 +0100

    Output disk uuid

--- output.tf
+++ output.tf
@@ -28,6 +28,10 @@ output "Windows-uuid" {
   value       = vsphere_virtual_machine.Windows.*.uuid
 }
 
+output "Windows-disks" {
+  value = { for d in flatten(vsphere_virtual_machine.Windows.*.disk) : d.label => d.uuid }
+}
+
 output "Linux-VM" {
   description = "VM Names"
   value       = vsphere_virtual_machine.Linux.*.name
@@ -46,4 +50,8 @@ output "Linux-guest-ip" {
 output "Linux-uuid" {
   description = "UUID of the VM in vSphere"
   value       = vsphere_virtual_machine.Linux.*.uuid
+}
+
+output "Linux-disks" {
+  value = { for d in flatten(vsphere_virtual_machine.Linux.*.disk) : d.label => d.uuid }
 }
