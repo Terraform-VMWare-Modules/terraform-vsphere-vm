a092f446c99574060be1b8d3e6dbd462c8be560c
Riccardo Salamanna <riccardo.salamanna@wefox.com>
2021-07-06 17:35:56 +0200

    fmt

--- tests/sanity/main.tf
+++ tests/sanity/main.tf
@@ -39,8 +39,8 @@ variable "vm" {
     memory_share_level = string
     memory_share_count = number
     io_reservation     = list(number)
-    io_share_level = list(string)
-    io_share_count = list(number)
+    io_share_level     = list(string)
+    io_share_count     = list(number)
   }))
 }
 
